ceb34afa27ed233f455ae3f6aa07c3b43d6297b9
emilsonsn <emilsonsn2@gmail.com>
2024-12-08 17:06:44 -0300

    feat: implementa models de actions e facilities

---
+++ src/app/models/action.ts
@@ -0,0 +1,17 @@
+export interface action {
+    id?: number;
+    component_id?: string;
+    name?: string;
+    type?: string;
+    date?: Date;
+    category?: string;
+    condition?: string;
+    priority?: string;
+    frequency?: string;
+    coast?: string;
+    curracy?: string;
+    description?: string;
+    image?: string;
+}
+  
+  
