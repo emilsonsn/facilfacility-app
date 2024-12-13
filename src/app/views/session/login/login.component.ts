5dd1d7103259cc34324a35ff4da4f4f89e9e883c
Clara <ClaraCosta@users.noreply.github.com>
2024-12-12 23:34:47 -0300

    {Fix} Ajustando redirecionamento pós login para a página de 'Facilities'

--- src/app/views/session/login/login.component.ts
+++ src/app/views/session/login/login.component.ts
@@ -55,7 +55,7 @@ export class LoginComponent {
 
       this._sessionQuery.user$.subscribe((user) => {
         this.user = user;
-        this._router.navigate(['/painel/home']);
+        this._router.navigate(['/painel/facility']);
       });
     }
   }
