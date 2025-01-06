563d82f23f6f1693f5dddb81385c465108d927d0
emilsonsn <emilsonsn2@gmail.com>
2025-01-06 20:54:38 -0300

    fix: adiciona api em enviroments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   appName: 'Facil Facility App',
   home: '/painel',  
-  api: 'https://facilfacility.com:3001',
+  api: 'https://facilfacility.com:3001/api',
   // api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
 };
