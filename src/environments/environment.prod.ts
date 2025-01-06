169b9a860a3c6d099d5ad40c8421d26395d26fcc
emilsonsn <emilsonsn2@gmail.com>
2025-01-06 20:46:29 -0300

    fix: ajusta enviroments do sistema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: false,
   appName: 'Facil Facility App',
   home: '/painel',
-  api: 'http://127.0.0.1:8000/api',
+  api: 'https://facilfacility.com:3001',
+  // api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
 };
